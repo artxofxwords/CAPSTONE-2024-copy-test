4209882a727ed9e8f2083a074b6ab8dc77013bc4
artxofxwords <brennan.amanda.j@gmail.com>
2024-07-09 14:50:19 -0400

    first push for wireframe no styling just layout and basic nav

---
+++ frontend/src/components/header/Login.jsx
@@ -0,0 +1,18 @@
+export default function Login() {
+  return (
+    <>
+      <div
+        style={{
+          width: "95vw",
+          height: "80vh",
+          backgroundColor: "gray",
+          color: "white",
+          textAlign: "center",
+          alignContent: "center",
+        }}
+      >
+        <p>This is where user logs in.</p>
+      </div>
+    </>
+  );
+}
---
+++ frontend/src/components/main/Landing.jsx
@@ -0,0 +1,14 @@
+import Header from "../header/Header.jsx";
+import Carousal from "./Carousal.jsx";
+
+export default function Landing () {
+
+
+    return (
+        <div>
+            <Header />
+
+            <Carousal />
+        </div>
+    )
+}
