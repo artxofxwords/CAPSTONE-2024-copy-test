7d5a36ae6b16cd7d9fcb350c49a44ec8d7af4945
BicoBean <ticobenedict@gmail.com>
2024-08-08 19:50:04 -0400

    gh pages v11

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,5 +6,5 @@ import vitePluginRequire from 'vite-plugin-require'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), vitePluginRequire.default()],
-  base: "/{CAPSTONE-2024/frontend}"
+  base: "/{frontend}"
 })
