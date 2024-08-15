44de6b31f6125696924dde770367ae0ebc25427e
artxofxwords <brennan.amanda.j@gmail.com>
2024-08-15 12:17:35 -0400

    testimonials style change

--- frontend/README.md
+++ frontend/README.md
@@ -1,21 +1,43 @@
-`Upright Capstone`
+# Upright Capstone
+***
 
-Capstone project website that recruits and hosts capstone sponsors and proposals.
+## Purpose of Project
+**Market, collect, and host sponsor proposals for Upright bootcamp capstone projects.**
 
-Frontend Stack
-//Node
-//Git Hub
-//MongoDB
-//React & Vite
-//Tailwind CC / Flowbite React
+## Technology and Software Used
+- Node
+- Express
+- MongoDB
+- React
+- Tailwind Flowbite (React)
+- JWT Decode
+- Nodemailer
+- GitPages
+- Netlify
+- Render
 
-Instructions
-For website maintenance, troubleshooting, and development.
+## Instructions for download and update
+1. Clone repo [CAPSTONE](https://github.com/TyBeen/CAPSTONE-2024.git) into local folder
+2. Create branch off develop01 `git checkout -b [newbranchname]`
+3. Split terminal, use `npm run dev` for backend (mongo) and frontend (vite)
+4. Push changes to capstone directory
+5. Use: 
+```
+git add .
+git commit -m "[message]"
+git push --set-upstream origin [thisbranchname]
+```
 
-Clone the repo: //url
-Split the terminal pane and open backend and frontend separately
-Run npm install on both ends to verify dependencies
-Run npm run dev on both ends to run the server and client
+## Instructions for deployment
+### FRONTEND
+Uses gitpages (module) and netlify (host)
+1. Push and merge all updates to _develop01_ branch in github
+2. Open _develop01_ branch locally in vscode and `git pull`
+3. Once local branch is up to date, test develop01 from `locahost:3000` for full functionality
+4. If the update runs perfectly from develop01 localhost, push and merge _develop01_ to prod in github
+5. Open _prod_ locally in vscode and `git pull`
+6. Once local branch is up to date, `npm run deploy` from _prod_
+>The live site is updated!
 
-Commit changes using git commands
-
+### BACKEND
+Uses render (host)
--- frontend/src/components/main/Testimonies.jsx
+++ frontend/src/components/main/Testimonies.jsx
@@ -4,87 +4,63 @@ export default function Testimonies () {
 <>
         <div className="grid grid-cols-3 gap-3">
   <div style={{
-                backgroundColor: "#1a9988",
-                color: "black",
-                border: "2px solid black",
-                borderRadius: "4px",
                 width: "25vw",
                 padding: "15px",
                 marginTop: "15px",
                 marginLeft: "50px",
                 marginBottom: "15px"
               }}>
-  <blockquote className="text-xl italic font-semibold text-black-900 dark:text-black"><p>SO HELPFUL!</p></blockquote>
+  <blockquote className="text-xl italic font-semibold text-black-900"><p>SO HELPFUL!</p></blockquote>
     <p>Super helpful! Bright minds willing to go the extra mile to get it exactly the way I wanted!</p> <p><b>- Small Business</b></p> </div>
 
 <div style={{
-                backgroundColor: "#1a9988",
-                color: "black",
-                border: "2px solid black",
-                borderRadius: "4px",
                 width: "25vw",
                 padding: "15px",
                 marginTop: "15px",
                 marginLeft: "15px",
                 marginBottom: "15px"
               }}>
-<blockquote className="text-xl italic font-semibold text-black-900 dark:text-black"><p>SO HELPFUL!</p></blockquote>
+<blockquote className="text-xl italic font-semibold text-black-900"><p>SO HELPFUL!</p></blockquote>
 The hectic life of a small business owner was vastly improved by working with this group of inventive minds! <p><b>- Small Business</b></p> </div>
 
 <div style={{
-                backgroundColor: "#1a9988",
-                color: "black",
-                border: "2px solid black",
-                borderRadius: "4px",
                 width: "25vw",
                 padding: "15px",
                 marginTop: "15px",
                 marginLeft: "15px",
                 marginBottom: "15px"
               }}>
-<blockquote className="text-xl italic font-semibold text-black-900 dark:text-black"><p>SO HELPFUL!</p></blockquote>
+<blockquote className="text-xl italic font-semibold text-black-900"><p>SO HELPFUL!</p></blockquote>
     Totally revamped my entire website to make it fresh and new! I cant thank them enough! <p><b>- Small Business</b></p> </div>
 
 <div style={{
-                backgroundColor: "#1a9988",
-                color: "black",
-                border: "2px solid black",
-                borderRadius: "4px",
                 width: "25vw",
                 padding: "15px",
                 marginTop: "15px",
                 marginLeft: "50px",
                 marginBottom: "15px"
               }}>
-<blockquote className="text-xl italic font-semibold text-black-900 dark:text-black"><p>SO HELPFUL!</p></blockquote>
+<blockquote className="text-xl italic font-semibold text-black-900"><p>SO HELPFUL!</p></blockquote>
     Super hard working! A great group who goes above and beyond! <p><b>- Small Business</b></p> </div>
 
 <div style={{
-                backgroundColor: "#1a9988",
-                color: "black",
-                border: "2px solid black",
-                borderRadius: "4px",
                 width: "25vw",
                 padding: "15px",
                 marginTop: "15px",
                 marginLeft: "15px",
                 marginBottom: "15px"
               }}>
-<blockquote className="text-xl italic font-semibold text-black-900 dark:text-black"><p>SO HELPFUL!</p></blockquote>
+<blockquote className="text-xl italic font-semibold text-black-900"><p>SO HELPFUL!</p></blockquote>
     A wonderful group! Always happy to help! <p><b>- Small Business</b></p> </div>
 
 <div style={{
-                backgroundColor: "#1a9988",
-                color: "black",
-                border: "2px solid black",
-                borderRadius: "4px",
                 width: "25vw",
                 padding: "15px",
                 marginTop: "15px",
                 marginLeft: "15px",
                 marginBottom: "15px"
               }}>
-<blockquote className="text-xl italic font-semibold text-black-900 dark:text-black"><p>SO HELPFUL!</p></blockquote>
+<blockquote className="text-xl italic font-semibold text-black-900"><p>SO HELPFUL!</p></blockquote>
     Completely changed the way we do business! Vast improvement over what we had before! <p><b>- Small Business</b></p> </div>
 </div>
         </>
