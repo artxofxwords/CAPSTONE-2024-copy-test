bbb7dd13612c9b0f281829d7aaa3d8b8ba07ea99
artxofxwords <brennan.amanda.j@gmail.com>
2024-08-15 12:26:40 -0400

    blockquote

--- frontend/src/components/main/Testimonies.jsx
+++ frontend/src/components/main/Testimonies.jsx
@@ -1,69 +1,133 @@
-export default function Testimonies () {
+export default function Testimonies() {
+  return (
+    <>
+      <div className="grid grid-cols-3 gap-3">
+        <div
+          style={{
+            width: "25vw",
+            padding: "15px",
+            marginTop: "15px",
+            marginLeft: "50px",
+            marginBottom: "15px",
+          }}
+        >
+          <blockquote className="p-4 my-4 border-s-4 border-gray-300 bg-gray-50">
+            <p className="text-xl italic font-semibold text-black-900">
+              SO HELPFUL!
+            </p>
+          </blockquote>
+          <p>
+            Super helpful! Bright minds willing to go the extra mile to get it
+            exactly the way I wanted!
+          </p>{" "}
+          <p>
+            <b>- Small Business</b>
+          </p>{" "}
+        </div>
 
-    return (
-<>
-        <div className="grid grid-cols-3 gap-3">
-  <div style={{
-                width: "25vw",
-                padding: "15px",
-                marginTop: "15px",
-                marginLeft: "50px",
-                marginBottom: "15px"
-              }}>
-  <blockquote className="text-xl italic font-semibold text-black-900"><p>SO HELPFUL!</p></blockquote>
-    <p>Super helpful! Bright minds willing to go the extra mile to get it exactly the way I wanted!</p> <p><b>- Small Business</b></p> </div>
+        <div
+          style={{
+            width: "25vw",
+            padding: "15px",
+            marginTop: "15px",
+            marginLeft: "15px",
+            marginBottom: "15px",
+          }}
+        >
+          <blockquote className="p-4 my-4 border-s-4 border-gray-300 bg-gray-50">
+            <p className="text-xl italic font-semibold text-black-900">
+              SO HELPFUL!
+            </p>
+          </blockquote>
+          The hectic life of a small business owner was vastly improved by
+          working with this group of inventive minds!{" "}
+          <p>
+            <b>- Small Business</b>
+          </p>{" "}
+        </div>
 
-<div style={{
-                width: "25vw",
-                padding: "15px",
-                marginTop: "15px",
-                marginLeft: "15px",
-                marginBottom: "15px"
-              }}>
-<blockquote className="text-xl italic font-semibold text-black-900"><p>SO HELPFUL!</p></blockquote>
-The hectic life of a small business owner was vastly improved by working with this group of inventive minds! <p><b>- Small Business</b></p> </div>
+        <div
+          style={{
+            width: "25vw",
+            padding: "15px",
+            marginTop: "15px",
+            marginLeft: "15px",
+            marginBottom: "15px",
+          }}
+        >
+          <blockquote className="p-4 my-4 border-s-4 border-gray-300 bg-gray-50">
+            <p className="text-xl italic font-semibold text-black-900">
+              SO HELPFUL!
+            </p>
+          </blockquote>
+          Totally revamped my entire website to make it fresh and new! I cant
+          thank them enough!{" "}
+          <p>
+            <b>- Small Business</b>
+          </p>{" "}
+        </div>
 
-<div style={{
-                width: "25vw",
-                padding: "15px",
-                marginTop: "15px",
-                marginLeft: "15px",
-                marginBottom: "15px"
-              }}>
-<blockquote className="text-xl italic font-semibold text-black-900"><p>SO HELPFUL!</p></blockquote>
-    Totally revamped my entire website to make it fresh and new! I cant thank them enough! <p><b>- Small Business</b></p> </div>
+        <div
+          style={{
+            width: "25vw",
+            padding: "15px",
+            marginTop: "15px",
+            marginLeft: "50px",
+            marginBottom: "15px",
+          }}
+        >
+          <blockquote className="p-4 my-4 border-s-4 border-gray-300 bg-gray-50">
+            <p className="text-xl italic font-semibold text-black-900">
+              SO HELPFUL!
+            </p>
+          </blockquote>
+          Super hard working! A great group who goes above and beyond!{" "}
+          <p>
+            <b>- Small Business</b>
+          </p>{" "}
+        </div>
 
-<div style={{
-                width: "25vw",
-                padding: "15px",
-                marginTop: "15px",
-                marginLeft: "50px",
-                marginBottom: "15px"
-              }}>
-<blockquote className="text-xl italic font-semibold text-black-900"><p>SO HELPFUL!</p></blockquote>
-    Super hard working! A great group who goes above and beyond! <p><b>- Small Business</b></p> </div>
+        <div
+          style={{
+            width: "25vw",
+            padding: "15px",
+            marginTop: "15px",
+            marginLeft: "15px",
+            marginBottom: "15px",
+          }}
+        >
+          <blockquote className="p-4 my-4 border-s-4 border-gray-300 bg-gray-50">
+            <p className="text-xl italic font-semibold text-black-900">
+              SO HELPFUL!
+            </p>
+          </blockquote>
+          A wonderful group! Always happy to help!{" "}
+          <p>
+            <b>- Small Business</b>
+          </p>{" "}
+        </div>
 
-<div style={{
-                width: "25vw",
-                padding: "15px",
-                marginTop: "15px",
-                marginLeft: "15px",
-                marginBottom: "15px"
-              }}>
-<blockquote className="text-xl italic font-semibold text-black-900"><p>SO HELPFUL!</p></blockquote>
-    A wonderful group! Always happy to help! <p><b>- Small Business</b></p> </div>
-
-<div style={{
-                width: "25vw",
-                padding: "15px",
-                marginTop: "15px",
-                marginLeft: "15px",
-                marginBottom: "15px"
-              }}>
-<blockquote className="text-xl italic font-semibold text-black-900"><p>SO HELPFUL!</p></blockquote>
-    Completely changed the way we do business! Vast improvement over what we had before! <p><b>- Small Business</b></p> </div>
-</div>
-        </>
-    )
-
+        <div
+          style={{
+            width: "25vw",
+            padding: "15px",
+            marginTop: "15px",
+            marginLeft: "15px",
+            marginBottom: "15px",
+          }}
+        >
+          <blockquote className="p-4 my-4 border-s-4 border-gray-300 bg-gray-50">
+            <p className="text-xl italic font-semibold text-black-900">
+              SO HELPFUL!
+            </p>
+          </blockquote>
+          Completely changed the way we do business! Vast improvement over what
+          we had before!{" "}
+          <p>
+            <b>- Small Business</b>
+          </p>{" "}
+        </div>
+      </div>
+    </>
+  );
 }
